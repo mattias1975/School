2bab58213ba5cdf3e388b33102c208101f9b8cc6
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-05-16 16:06:16 +0200

    16 maj

---
+++ School/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string  studentfirstName { get; set; }
+        public string studentlastName { get; set; }
+        public string course { get; set; }
+        public int grade { get; }
+    }
---
+++ School/Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School.Models
+{ 
+    public class Teacher : Student;
+{
+    public int Id { get; set }
+    public string studstudentfirstNameentn { get; set; }
+    public string studstudentlastNameentn { get; set; }
+        
+
+}
+}
